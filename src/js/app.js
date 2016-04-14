c04810ecbdc73c54f9a46574682cf891cf8db7cd
Kaeserkiev <kaeserkiev@gmail.com>
2016-04-14 07:31:32 +0300

    second

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+//Common scripts
+
+
+//ie fix
+if(!(window.console && console.log)) {
+	console = {
+		log: function(){},
+		debug: function(){},
+		info: function(){},
+		warn: function(){},
+		error: function(){}
+	};
+}
+
+$(function () {
+
+
+});
