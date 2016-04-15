87a2400fc3d39f851b192a8abcb45e4cb607c211
Kaeserkiev <kaeserkiev@gmail.com>
2016-04-15 14:33:34 +0300

    add markup for two pages

--- product/js/app.js
+++ product/js/app.js
@@ -12,8 +12,29 @@ if(!(window.console && console.log)) {
 	};
 }
 
-$(function () {
 
+(function () {
 
-});
+	/*setup custom select*/
+	$("#select-country").select2({
+		placeholder: 'Choose your country',
+		minimumResultsForSearch: Infinity, //hide search
+		closeOnSelect: true
+	});
+
+	/*setup scrollspy for Stores page*/
+	$(window).on('scroll', function() {
+		var scrollTop = $(this).scrollTop();
+
+		$('.stores__title').each(function() {
+			var topDistance = $(this).offset().top;
+
+			if ( (topDistance) < scrollTop + 150 ) {
+				$(this).addClass('stores__title_active');
+			} else {
+				$(this).removeClass('stores__title_active');
+			}
+		});
+	});
+})(jQuery);
 
